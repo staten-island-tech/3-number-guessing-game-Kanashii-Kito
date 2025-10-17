145e14758036c2a95ea4e9db07a58633010fef6e
Kanashii-Kito <ERicc468@nycstudents.net>
2025-10-17 10:01:08 -0400

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,13 @@
+count = 1
+
+while count <= 5:
+    print("This is loop number", count)
+    count = count + 1
+
+
+order = ""
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!")
