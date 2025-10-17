40702d541ad68a460bc9cae0ec092ef989c934c5
Kanashii-Kito <ERicc468@nycstudents.net>
2025-10-17 10:11:07 -0400

    Update test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+# count = 1
+
+# while count <= 5:
+#     print("This is loop number", count)
+#     count = count + 1
+
+# order = ""
+
+# while order != "done":
+#     order = input("What would you like to order? (type 'done' to finish): ")
+
+# print("Thanks for your order!")
+
+'''------------------------------------------------------------------------------------'''
+'''Optional Challenge 2'''
+
+# color = ""
+
+# while color != "Stop.":
+#     color = input("What is your favorite color? (Type |Stop.| when done.):  ")
+# print ("Thank you for stating your favorite color along with everything about you.")
