8689a1a0555b241d74964df4d2300686cbe0c2ff
Kanashii-Kito <ERicc468@nycstudents.net>
2025-10-21 09:39:56 -0400

    Update test.py

--- test.py
+++ test.py
@@ -33,7 +33,10 @@ import random
 
 ran = random.randint(1, 10)
 
+
 user_input = None
+
+
 
 while user_input != ran:
     user_input = int(input("Please guess the random number selected from 1 to 10: "))
@@ -41,9 +44,15 @@ while user_input != ran:
         print("Wrong number! Try again. Your number is less than the correct number.")
     elif user_input > ran:
         print ("Wrong number Try again. Your number is greater than the correct number.")
+    x= []
+    user_input.append(x)
+
+
+
+
 
 print("Thank you for guessing the correct number!")
-
+print (f"Your guess history is below: {x}")
 '''------------------------------------------------------------------------------------------------------------------------------------------------'''
 
 #     user_input = input(f"Please input a number from 1 to 10: ")
